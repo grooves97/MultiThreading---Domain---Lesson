78da2baa317a9f2f6ac43c978573a1775be5be52
Аслан Нурсеитов <grooves97@gmail.com>
2019-05-28 10:46:37 +0600

    Приложение запуска через Domain, с приложением CalculatorSum.exe

---
+++ CalculatorSum/CalculatorSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                var firsNumber = int.Parse(args[0]);
+                var secondNumber = int.Parse(args[1]);
+
+                Console.WriteLine($"Сумма из приложения калькулятора = {firsNumber + secondNumber}");
+            }
+        }
+    }
+}
---
+++ CreateAnotherDomain/CreateAnotherDomain/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAnotherDomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDomain = AppDomain.CurrentDomain;
+            ShowDomainInfo(currentDomain);
+
+            var calculatorDomain = AppDomain.CreateDomain("Calculator");
+            ShowDomainInfo(calculatorDomain);
+
+            string assemblyForCalculatorDomain = currentDomain.BaseDirectory + "CalculatorSum.exe";
+            calculatorDomain.ExecuteAssembly(assemblyForCalculatorDomain, new string[] { "10", "15" });
+            ShowDomainInfo(calculatorDomain);
+
+            AppDomain.Unload(calculatorDomain);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowDomainInfo(AppDomain domain)
+        {
+            var sortedAsseblies = domain.
+                GetAssemblies().
+                OrderBy(assembly => assembly.
+                GetName().Name);
+
+            Console.WriteLine($"***{domain.FriendlyName}***");
+            foreach (var assembly in sortedAsseblies)
+            {
+                Console.WriteLine($"{assembly.GetName().Name} - {assembly.Location}");
+            }
+        }
+    }
+}
